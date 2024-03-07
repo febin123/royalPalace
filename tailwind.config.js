8cd8746270e0c108e7b90d423bebb88d9aaba8aa
febin123 <febsep96@gmail.com>
2024-03-08 03:45:06 +0530

    added navbar compoenent

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        brightColor:"#F4511F",
+        backgroundColor:"#b7bca9",
+        lightText:"#959595"
+      }
+    },
   },
   plugins: [],
 }
